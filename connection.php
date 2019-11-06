712154b16ce074f2ff7a390b534364069bf49985
james ross <james_ross@outlook.fr>
2019-11-06 20:29:56 +0100

    all good

--- connection.php
+++ connection.php
@@ -7,16 +7,9 @@ $dbUserName = $user;
 $dbPassword = $pass;
 $dbName = 'TnuAWjwlHS';
 
-// $dbServerName = "remotemysql.com";
-// $dbUserName = "TnuAWjwlHS";
-// $dbPassword = "SaXj67gCa7";
-// $dbName = "TnuAWjwlHS";
-
 $conn = new PDO('mysql:host=remotemysql.com;dbname=TnuAWjwlHS', $dbUserName, $dbPassword);
 
 // $dbServerName = "localhost";
 // $dbUserName = "root";
 // $dbPassword = "";
 // $dbName = "jeuxvideo";
-
-// $conn2 = new PDO('mysql:host=localhost;dbname=jeuxvideo', $dbUserName, $dbPassword);
